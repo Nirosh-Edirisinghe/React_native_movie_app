e66de330c3a876bbcfbc1b38fec5d69e96a204c6
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-10-20 22:00:03 +0530

    create navigation tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
+
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center">
-      <Text className="text-5xl text-red-500">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-5xl text-light-200">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
